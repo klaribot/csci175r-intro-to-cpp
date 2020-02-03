2c25be5248656329a347da041381a512582c984d
klaribot <crystaltheandroid@gmail.com>
2020-02-03 13:00:53 -0500

    Getting ahead of myself :P

---
+++ Week 3/Final Project/bookinfo.cpp
@@ -0,0 +1,19 @@
+//Booksellers Final Project - Week 1 - Justin Reed
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Booksellers" << endl;
+    cout << "Inventory Database" << endl << endl;
+
+    cout << "ISBN:" << endl;
+    cout << "Title:" << endl;
+    cout << "Author:" << endl;
+    cout << "Publisher:" << endl;
+    cout << "Date Added:" << endl;
+    cout << "Quantity-On-Hand:" << endl;
+    cout << "Wholesale Cost:" << endl;
+    cout << "Retail Price:" << endl;
+
+}
