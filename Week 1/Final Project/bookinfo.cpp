10f12f13755fb2c3cf3f2b61a4a2bc613fdf7aff
klaribot <crystaltheandroid@gmail.com>
2020-02-03 11:55:03 -0500

    Menus Finished, Moving on to Chapter 2

--- Week 1/Final Project/bookinfo.cpp
+++ Week 1/Final Project/bookinfo.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
+    cout << "Booksellers" << endl;
+    cout << "Inventory Database" << endl << endl;
+
+    cout << "ISBN:" << endl;
+    cout << "Title:" << endl;
+    cout << "Author:" << endl;
+    cout << "Publisher:" << endl;
+    cout << "Date Added:" << endl;
+    cout << "Quantity-On-Hand:" << endl;
+    cout << "Wholesale Cost:" << endl;
+    cout << "Retail Price:" << endl;
 
 }
