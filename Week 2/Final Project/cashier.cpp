1a9e550cade1ecb9487e7bc789e05911284a1996
klaribot <crystaltheandroid@gmail.com>
2020-02-03 11:56:57 -0500

    Week 2 Started

---
+++ Week 2/Final Project/cashier.cpp
@@ -0,0 +1,20 @@
+//Booksellers Final Project - Week 1 - Justin Reed
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Booksellers" << endl << endl;
+
+    cout << "Date:" << endl << endl;
+
+    cout << "Qty   ISBN\t\tTitle\t\t\t\tPrice\t\tTotal" << endl;
+    cout << "_____________________________________________________________________________" << endl << endl << endl;
+
+
+    cout << "\tSubtotal" << endl;
+    cout << "\tTax" << endl;
+    cout << "\tTotal" << endl << endl;
+
+    cout << "Thank You for shopping at Booksellers!" << endl;
+}
