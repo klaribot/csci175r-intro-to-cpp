0c86f5ce3c6437b23c4dfb9bf3f00e792a85703e
klaribot <crystaltheandroid@gmail.com>
2020-03-09 13:01:44 -0400

    Playing catch-up

---
+++ Week 4/Final Project/cashier.cpp
@@ -0,0 +1,44 @@
+//Booksellers Final Project - Week 1 - Justin Reed
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int quantity;
+    float unitPrice, totalPerEntry;
+    string date, isbn, title;
+
+    cout << "Booksellers" << endl;
+    cout << " Cashier Module" << endl << endl;
+
+    // Prompt user for shopping information
+    cout << "Date (MM/DD/YY): ";
+    cin >> date;
+    cout << "Quantity of Book: ";
+    cin >> quantity;
+    cout << "ISBN: ";
+    cin >> isbn;
+    cout << "Title: ";
+    cin >> title;
+    cout << "Price: ";
+    cin >> unitPrice;
+    cout << endl;
+
+    // Calculate total per each book entry
+    totalPerEntry = quantity * unitPrice;
+
+    cout << "Booksellers" << endl << endl;
+
+    cout << "Date: " << date << endl << endl;
+
+    cout << "Qty\tISBN\t\tTitle\t\t\t\tPrice\t\tTotal" << endl;
+    cout << "_____________________________________________________________________________" << endl;
+    cout << quantity << "\t" << isbn << "\t\t" << title << "\t\t\t" << unitPrice << "\t\t" << totalPerEntry << endl << endl;
+
+    cout << "\tSubtotal" << endl;
+    cout << "\tTax" << endl;
+    cout << "\tTotal" << endl << endl;
+
+    cout << "Thank You for shopping at Booksellers!" << endl;
+}
---
+++ Week 4/Final Project/reports.cpp
@@ -0,0 +1,22 @@
+//Booksellers Final Project - Week 1 - Justin Reed
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short int choice;
+
+    cout << "Booksellers" << endl;
+    cout << "Reports" << endl << endl;
+
+    cout << "1. Inventory listing" << endl;
+    cout << "2. Inventory wholesale value" << endl;
+    cout << "3. Inventory resale value" << endl;
+    cout << "4. Listing by Quantity" << endl;
+    cout << "5. Listing by Cost" << endl;
+    cout << "6. Listing by Age" << endl;
+    cout << "7. Return to Main Menu" << endl << endl;
+
+    cout << "Enter your choice: ";
+    cin >> choice;
+}
